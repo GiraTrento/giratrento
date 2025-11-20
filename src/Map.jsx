caff3816fd419a3f5aed7b09d2ee509a1aab0b9e
mattefwe <matterisco@gmail.com>
2025-11-20 12:00:48 +0100

    fix: changed map

--- src/Map.jsx
+++ src/Map.jsx
@@ -9,25 +9,25 @@ import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41]
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const MyMap = () => {
-    const position = [46.069692, 11.121089];
+  const position = [46.069692, 11.121089];
 
-    return (
-        <MapContainer center={position} zoom={14} scrollWheelZoom={true} className="leaflet-container">
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-        </MapContainer>
-    );
+  return (
+    <MapContainer center={position} zoom={14} scrollWheelZoom={true} className='leaflet-container'>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+      />
+    </MapContainer>
+  );
 };
 
 export default MyMap;
