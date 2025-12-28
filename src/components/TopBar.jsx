5f85c24c57104fd300abcbb35e7513abd3f1f5d8
mattefwe <matterisco@gmail.com>
2025-12-28 11:41:04 +0100

    feat: added topbar icon animations

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -17,10 +17,16 @@ const TopBar = ({ menuOpened, openMenu, filtersOpened, openFilters }) => {
             strokeLinecap='round'
             strokeLinejoin='round'
           >
-            <line x1='4' y1='4' x2='40' y2='4'></line>
-            <line x1='4' y1='14' x2='30' y2='14'></line>
-            <line x1='4' y1='24' x2='40' y2='24'></line>
-            <line x1='4' y1='34' x2='30' y2='34'></line>
+            <g className='icon-original'>
+              <line x1='4' y1='4' x2='40' y2='4'></line>
+              <line x1='4' y1='14' x2='30' y2='14'></line>
+              <line x1='4' y1='24' x2='40' y2='24'></line>
+              <line x1='4' y1='34' x2='30' y2='34'></line>
+            </g>
+            <g className='icon-close'>
+              <line x1='10' y1='6' x2='34' y2='30'></line>
+              <line x1='10' y1='30' x2='34' y2='6'></line>
+            </g>
           </svg>
         </button>
       </div>
@@ -41,12 +47,18 @@ const TopBar = ({ menuOpened, openMenu, filtersOpened, openFilters }) => {
             strokeLinecap='round'
             strokeLinejoin='round'
           >
-            <line x1='6' y1='4' x2='6' y2='10'></line>
-            <line x1='34' y1='4' x2='34' y2='10'></line>
-            <line x1='6' y1='10' x2='15' y2='22'></line>
-            <line x1='34' y1='10' x2='25' y2='22'></line>
-            <line x1='15' y1='22' x2='15' y2='34'></line>
-            <line x1='25' y1='22' x2='25' y2='34'></line>
+            <g className='icon-original'>
+              <line x1='6' y1='4' x2='6' y2='10'></line>
+              <line x1='34' y1='4' x2='34' y2='10'></line>
+              <line x1='6' y1='10' x2='15' y2='22'></line>
+              <line x1='34' y1='10' x2='25' y2='22'></line>
+              <line x1='15' y1='22' x2='15' y2='34'></line>
+              <line x1='25' y1='22' x2='25' y2='34'></line>
+            </g>
+            <g className='icon-close'>
+              <line x1='8' y1='6' x2='32' y2='30'></line>
+              <line x1='8' y1='30' x2='32' y2='6'></line>
+            </g>
           </svg>
         </button>
       </div>
